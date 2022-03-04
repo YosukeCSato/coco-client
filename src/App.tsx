8585c559a5a336ff2780211c9e0e58c30e5f4c54
YosukeCSato <yosuke.c.sato@gmail.com>
2022-03-05 03:41:23 +0900

    apiに通信して文字列を表示させられるようにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [takagi, setTakagi] = useState('');
+  useEffect(() => {
+    fetch('http://localhost:8080/api/')
+      .then((res) => res.json())
+      .then((data) => setTakagi(data.takagi));
+  }, []);
+
+  console.log('ahan');
+  console.log({ takagi });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{takagi}</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
